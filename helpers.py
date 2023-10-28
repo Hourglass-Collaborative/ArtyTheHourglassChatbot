bd6510184446defd5839360563a5b4c8b92ccfa7
Tyler Shannon <tylershannon@Tylers-iMac.local>
2023-10-28 18:02:11 -0400

    spacy dependancy

--- helpers.py
+++ helpers.py
@@ -5,16 +5,14 @@ import en_core_web_trf
 nlp = en_core_web_trf.load()
 from nltk import pos_tag
 from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-import nltk
+#from nltk.corpus import stopwords
+#import nltk
 
-with open("english.txt") as file:
-    lines = [line.rstrip() for line in file]
+stop_words = set(['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"])
 
-stop_words = set(lines)
 
-#stop_words = set(stopwords.words("english"))
 stop_words.add("hourglass")
+
 def preprocess(input_sentence):
     input_sentence = input_sentence.lower()
     input_sentence = re.sub(r'[^\w\s]','',input_sentence)
